5fdc2bfdbb9d6a5f6319226e152fb57f8a223c19
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2024-12-28 04:21:23 +0000

    Making it work on .NET 7

---
+++ api/IssueWalletTrigger.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Sebug.Function
+{
+    public class IssueWalletTrigger
+    {
+        private readonly ILogger<IssueWalletTrigger> _logger;
+
+        public IssueWalletTrigger(ILogger<IssueWalletTrigger> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("IssueWalletTrigger")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
