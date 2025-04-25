e5a56fd4e87c018256194cbf536d9eb41565c115
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2025-04-25 15:43:54 +0000

    Adding a bit more files

--- api/Models/Pass.cs
+++ api/Models/Pass.cs
@@ -2,7 +2,9 @@ namespace Sebug.Function.Models;
 
 public record Pass(string organizationName,
 string passTypeIdentifier,
-int formatVersion = 1)
+int formatVersion = 1,
+string description = "E-TICKET",
+string passTypeIdentifier = "ch.sebug.passes.ticket")
 {
     
 }
