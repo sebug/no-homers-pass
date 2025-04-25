143f891c6b99cce52967200f28822fbbf4de9dc7
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2025-04-25 15:46:08 +0000

    Avoiding duplicate property

--- api/Models/Pass.cs
+++ api/Models/Pass.cs
@@ -3,8 +3,7 @@ namespace Sebug.Function.Models;
 public record Pass(string organizationName,
 string passTypeIdentifier,
 int formatVersion = 1,
-string description = "E-TICKET",
-string passTypeIdentifier = "ch.sebug.passes.ticket")
+string description = "E-TICKET")
 {
     
 }
