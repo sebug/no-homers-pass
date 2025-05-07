6c21dff968b071cd4c4f97901bb86a7c14a5f52c
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2025-05-07 10:03:38 +0000

    Trying with httpresponsedata

---
+++ api/CustomHeaderResponseTrigger.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Sebug.Function
+{
+    public class CustomHeaderResponseTrigger
+    {
+        private readonly ILogger<CustomHeaderResponseTrigger> _logger;
+
+        public CustomHeaderResponseTrigger(ILogger<CustomHeaderResponseTrigger> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("CustomHeaderResponseTrigger")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.Headers.Add("Last-Modified", "Tuesday");
+            response.WriteString("Maybe custom header??");
+            return response;
+        }
+    }
+}
